6a862e7f75e7c6b67d58992183b73e04f50348ea
Rahul <rsmawari198@gmail.com>
2018-10-28 06:13:39 +0530

    make server

---
+++ server/server.py
@@ -0,0 +1,30 @@
+
+from flask import Flask
+import mausam
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from flask import jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/<name>')
+def hello(name):
+	return name
+
+@app.route('/api/weather/<query>')
+def Weather(query):
+	res = mausam.result(query)
+	return res
+
+@app.route('/api/price/<query>')
+def Price(query):
+	res = Market_price.result(query)
+	return res
+
+ 
+
+if __name__ == '__main__':
+	app.run(debug=True)
